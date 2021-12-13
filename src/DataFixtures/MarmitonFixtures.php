6d00546400241208bc8af642f6a16e2d0fba55bd
Mael Jamin <mael.jamin@gmail.com>
2021-12-13 19:40:30 +0100

    MAJ Marmiton.js et marmiton_results.json + modif entity Recipe

---
+++ src/DataFixtures/MarmitonFixtures.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Recipe;
+use App\Entity\User;
+use Cassandra\Time;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class MarmitonFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        /**** LECTURE DU JSON ****/
+        $string = file_get_contents("marmiton_results.json");
+        if ($string === false) {
+            throw new \Error("Erreur lecture fichier");
+        }
+
+        $json_a = json_decode($string, true);
+        if ($json_a === null) {
+            throw new \Error("Conversion JSON impossible");
+        }
+        /**** FIN LECTURE DU JSON ****/
+
+        //Je récupère chaque object json qui représente une recette
+        foreach ($json_a as $recipes_json => $recipe_json) {
+            $total_time = intdiv($recipe_json["prepTime"], 60).':'. ($recipe_json["prepTime"] % 60);
+            $preparation_time = intdiv($recipe_json["totalTime"], 60).':'. ($recipe_json["totalTime"] % 60);
+            $recipe = new Recipe();
+            $recipe
+                ->setUserId($manager->getRepository(User::class)->findBy(['email'=>'admin@gmail.com'])[0])
+                ->setName($recipe_json["name"])
+                ->setUrl($recipe_json["url"])
+                ->setType("Entree")
+                ->setNumberOfPersons($recipe_json["people"])
+                ->setDifficulty($recipe_json["difficulty"])
+                ->setPreparationTime(new \DateTime($preparation_time))
+                ->setTotalTime(new \DateTime($total_time))
+                ->setCreatedAt(new \DateTimeImmutable());
+            $manager->persist($recipe);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class,
+        ];
+    }
+}
