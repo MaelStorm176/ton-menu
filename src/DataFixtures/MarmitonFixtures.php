a47492808fd73edd66eda6d6d54f6a7f60f51f8f
Mael Jamin <mael.jamin@gmail.com>
2021-12-21 19:33:03 +0100

    Ajout des ingredients depuis le fichier marmiton_results.json

--- src/DataFixtures/MarmitonFixtures.php
+++ src/DataFixtures/MarmitonFixtures.php
@@ -2,9 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ingredient;
 use App\Entity\Recipe;
 use App\Entity\User;
-use Cassandra\Time;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -13,7 +13,9 @@ class MarmitonFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        $user_entity = $manager->getRepository(User::class)->findOneBy(['email'=>'admin@gmail.com']);
         $types_recettes = ["ENTREE","PLAT","DESSERT"];
+        $ingredient_name_list = [];
 
         /**** LECTURE DU JSON ****/
         $string = file_get_contents("marmiton_results.json");
@@ -30,11 +32,12 @@ class MarmitonFixtures extends Fixture implements DependentFixtureInterface
         //Je récupère chaque object json qui représente une recette | 0=Entrées, 1=Plats, 2=Desserts
         for ($i=0;$i<3;$i++){
             foreach ($json_a[$i] as $recipe_json) {
+                /**** RECIPE ****/
                 $total_time = intdiv($recipe_json["totalTime"], 60).':'. ($recipe_json["totalTime"] % 60);
                 $preparation_time = intdiv($recipe_json["prepTime"], 60).':'. ($recipe_json["prepTime"] % 60);
                 $recipe = new Recipe();
                 $recipe
-                    ->setUserId($manager->getRepository(User::class)->findBy(['email'=>'admin@gmail.com'])[0])
+                    ->setUserId($user_entity)
                     ->setName($recipe_json["name"])
                     ->setUrl($recipe_json["url"])
                     ->setType($types_recettes[$i])
@@ -44,6 +47,23 @@ class MarmitonFixtures extends Fixture implements DependentFixtureInterface
                     ->setTotalTime(new \DateTime($total_time))
                     ->setCreatedAt(new \DateTimeImmutable());
                 $manager->persist($recipe);
+                /******************************************************/
+                /**** INGREDIENTS ****/
+                $ingredient_liste = explode(",",$recipe_json["description"]);
+                foreach ($ingredient_liste as $ingredient_name){
+                    $ingredient_name = trim($ingredient_name);
+                    if(!isset($ingredient_name_list[$ingredient_name])){
+                        $ingredient_name_list[$ingredient_name] = true;
+                        $ingredient = new Ingredient();
+                        $ingredient
+                            ->setUserId($user_entity)
+                            ->setName($ingredient_name)
+                            ->setPrimaryType("marmiton")
+                            ->setSecondaryType("marmiton")
+                            ->setCreatedAt(new \DateTimeImmutable());
+                        $manager->persist($ingredient);
+                    }
+                }
             }
         }
         $manager->flush();
