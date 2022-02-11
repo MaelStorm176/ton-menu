58ed19dc374214dfa4e919f5233fac5c574f4d11
Raph <thesket5@outlook.fr>
2022-02-11 15:26:01 +0100

    Add the menu generation and generation of one recipe random

---
+++ migrations/Version20220117202141.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220117202141 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, recette_id INT DEFAULT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_9474526CA76ED395 (user_id), INDEX IDX_9474526C89312FE9 (recette_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ingredient (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, name VARCHAR(40) NOT NULL, primary_type VARCHAR(20) NOT NULL, secondary_type VARCHAR(20) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_6BAF78705E237E06 (name), INDEX IDX_6BAF78709D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rating (id INT AUTO_INCREMENT NOT NULL, recette_id INT DEFAULT NULL, user_id INT DEFAULT NULL, rate VARCHAR(255) NOT NULL, INDEX IDX_D889262289312FE9 (recette_id), INDEX IDX_D8892622A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, name VARCHAR(255) NOT NULL, url VARCHAR(255) DEFAULT NULL, type VARCHAR(20) NOT NULL, number_of_persons SMALLINT NOT NULL, difficulty DOUBLE PRECISION NOT NULL, preparation_time INT NOT NULL, total_time INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_DA88B1379D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe_ingredients (id INT AUTO_INCREMENT NOT NULL, recipe_id INT NOT NULL, ingredient_id INT NOT NULL, INDEX IDX_9F925F2B59D8A214 (recipe_id), INDEX IDX_9F925F2B933FE08C (ingredient_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe_steps (id INT AUTO_INCREMENT NOT NULL, recipe_id INT NOT NULL, step LONGTEXT NOT NULL, INDEX IDX_2231DE6D59D8A214 (recipe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe_tags (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(150) NOT NULL, UNIQUE INDEX UNIQ_10A7CEF95E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe_tags_links (id INT AUTO_INCREMENT NOT NULL, recipe_id INT NOT NULL, recipe_tag_id INT NOT NULL, INDEX IDX_F398D12359D8A214 (recipe_id), INDEX IDX_F398D12337CC7D30 (recipe_tag_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C89312FE9 FOREIGN KEY (recette_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE ingredient ADD CONSTRAINT FK_6BAF78709D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D889262289312FE9 FOREIGN KEY (recette_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D8892622A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B1379D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE recipe_ingredients ADD CONSTRAINT FK_9F925F2B59D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE recipe_ingredients ADD CONSTRAINT FK_9F925F2B933FE08C FOREIGN KEY (ingredient_id) REFERENCES ingredient (id)');
+        $this->addSql('ALTER TABLE recipe_steps ADD CONSTRAINT FK_2231DE6D59D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE recipe_tags_links ADD CONSTRAINT FK_F398D12359D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE recipe_tags_links ADD CONSTRAINT FK_F398D12337CC7D30 FOREIGN KEY (recipe_tag_id) REFERENCES recipe_tags (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe_ingredients DROP FOREIGN KEY FK_9F925F2B933FE08C');
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C89312FE9');
+        $this->addSql('ALTER TABLE rating DROP FOREIGN KEY FK_D889262289312FE9');
+        $this->addSql('ALTER TABLE recipe_ingredients DROP FOREIGN KEY FK_9F925F2B59D8A214');
+        $this->addSql('ALTER TABLE recipe_steps DROP FOREIGN KEY FK_2231DE6D59D8A214');
+        $this->addSql('ALTER TABLE recipe_tags_links DROP FOREIGN KEY FK_F398D12359D8A214');
+        $this->addSql('ALTER TABLE recipe_tags_links DROP FOREIGN KEY FK_F398D12337CC7D30');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE ingredient');
+        $this->addSql('DROP TABLE rating');
+        $this->addSql('DROP TABLE recipe');
+        $this->addSql('DROP TABLE recipe_ingredients');
+        $this->addSql('DROP TABLE recipe_steps');
+        $this->addSql('DROP TABLE recipe_tags');
+        $this->addSql('DROP TABLE recipe_tags_links');
+    }
+}
