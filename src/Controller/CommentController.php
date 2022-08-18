d043ea47ca9f2df286817377c84760dbbe6cbdcf
Raph Bess <thesket5@outlook.fr>
2022-08-18 14:33:24 +0200

    Fix user role
    - Block all route user don't need to go
    - Check if all is good
    - Add new entrypoint for error access denied

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Repository\SignalementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     #[Route('/delete/{id}', name: 'delete_comment')]
-    public function delete(Comment $comment): Response{
+    public function delete(Comment $comment, SignalementRepository $signalementRepository): Response{
         $user = $this->getUser();
         if(in_array("ROLE_ADMIN", $user->getRoles()) XOR $user->getId() == $comment->getUser()->getId()){
+            $signalement = $signalementRepository->findOneBy(['message' => $comment]);
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($comment);
+            $entityManager->remove($signalement, $comment);
             $entityManager->flush();
             return $this->redirectToRoute('recipe_show', [
                 'id' => $comment->getRecette()->getId(),
